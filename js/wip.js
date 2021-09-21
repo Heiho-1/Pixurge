6f111606c9a927686f58f869852fc72fdbe10058
Heiho <heiho@cock.li>
2021-09-21 12:00:23 -0500

    a

--- js/wip.js
+++ js/wip.js
@@ -1,7 +1,34 @@
-// going to use this for textures
+function render(){
+for(let i = 0; i < game.blocks.length; i++){
+for(let p = 0; p < game.blocks[i].length; p++){
 
-function addTexture(texture,x,y){
-let thisTexture = new Image(x,y)
-
-
+if(typeof game.blocks[i][p] == "string"){
+	if( game.blocks[i][p].substr(0,8) == "texture:"){
+let textaddress = game.blocks[i][p].substr(8,Infinity)
+let img = new Image(game.blocks[i][2],game.blocks[i][2])
+img.src = textaddress
+game.blocks[i]["texture"] = img
+game.blocks[i][p] = ""
 }
+	if(game.blocks[i][p].substr(0,5) == "type:"){
+   switch(game.blocks[i][p].substr(5,Infinity)){
+   case 'sanic':
+   game.blocks[i][0] += game.magicnum
+   game.blocks[i][1] += game.magicnum
+   game.blocks[i][p] = 'type:sanic1'
+   break;   
+   case 'sanic1':
+   game.blocks[i][0] -= game.magicnum
+   game.blocks[i][1] -= game.magicnum
+       game.blocks[i][p] = 'type:sanic'
+
+   break;
+
+   }
+}
+}
+}
+}}
+
+
+setInterval(render,2000)
